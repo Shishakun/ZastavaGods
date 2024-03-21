1111c41ea7bfb5a1f2a84452d639f367d87ac5d7
Shisha <shishkarev16@gmail.com>
2024-03-21 13:51:16 +0300

    +fix

--- fastApi/inputs/facerecognition.py
+++ fastApi/inputs/facerecognition.py
@@ -15,9 +15,8 @@ def face_confidence(face_distance, face_match_threshold=0.6):
         return str(round(linear_val * 100, 2)) + "%"
     else:
         value = (
-                        linear_val
-                        + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))
-                ) * 100
+            linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))
+        ) * 100
         return str(round(value, 2)) + "%"
 
 
@@ -33,114 +32,68 @@ class FaceRecognition:
     face_names = []
     known_face_encodings = []
     known_face_names = []
-    process_current_frame = True
 
     def __init__(self):
         self.encode_faces()
 
     def encode_faces(self):
         # Load images from 'people' directory
-        for image in os.listdir('./inputs/people'):
-            face_image = face_recognition.load_image_file(f'./inputs/people/{image}')
-            face_encoding = face_recognition.face_encodings(face_image)[
-                0]
-            logger.debug(face_encoding)
-            self.known_face_encodings.append(face_encoding)
+        for image in os.listdir("../inputs/people"):
+            face_image = face_recognition.load_image_file(f"../inputs/people/{image}")
+            self.face_encoding = face_recognition.face_encodings(face_image)[0]
+            logger.debug(self.face_encoding)
+
+            self.known_face_encodings.append(self.face_encoding)
+            logger.debug(self.known_face_encodings)
+
             self.known_face_names.append(image)
             logger.debug(self.known_face_names)
 
-    def run_recognition(self):
-        # Initialize video capture
-        video_capture_face_recognition = cv2.VideoCapture(0)
-        logger.debug('Камера запущена')
-        # Initialize consecutive detections counter
-        logger.debug(video_capture_face_recognition)
-        consecutive_detections = 0
+    def detect_faces_in_image(self, image):
+        # Convert the image to RGB format
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        logger.debug("Изображение принято")
+        # Detect faces
+        face_locations = face_recognition.face_locations(rgb_image)
+        logger.debug(face_locations)
+        face_encodings = face_recognition.face_encodings(rgb_image, face_locations)
+        logger.debug(face_encodings)
 
-        while True:
-            ret, frame = video_capture_face_recognition.read()
+        return face_locations, face_encodings
 
-            if self.process_current_frame:
-                # Resize frame
-                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    def run_recognition(self, image=None):
+        self.face_locations, self.face_encodings = self.detect_faces_in_image(image)
+        if image:
+            face_names = []
 
-                # Convert to RGB
-                rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
-
-                # Detect faces
-                self.face_locations = face_recognition.face_locations(
-                    rgb_small_frame)
-                self.face_encodings = face_recognition.face_encodings(
-                    rgb_small_frame, self.face_locations
+            for face_encoding in self.face_encodings:
+                matches = face_recognition.compare_faces(
+                    self.known_face_encodings, face_encoding
                 )
 
-                # Update consecutive detections counter
-                if len(self.face_encodings) > 0:
-                    consecutive_detections += 1
-                else:
-                    consecutive_detections = 0
+                # Initialize name and confidence
+                name = "unknown"
+                confidence = "unknown"
 
-                # Check if access should be granted
-                if consecutive_detections >= 10:
-                    logger.error("ДОСТУП РАЗРЕШЕН")
-
-                # Initialize face names list
-                face_names = []
-
-                for face_encoding in self.face_encodings:
-                    matches = face_recognition.compare_faces(
-                        self.known_face_encodings, face_encoding)
-
-                    # Initialize name and confidence
-                    name = "unknown"
-                    confidence = "unknown"
-
-                    # Find matches
-                    face_distances = face_recognition.face_distance(
-                        self.known_face_encodings, face_encoding
-                    )
-                    best_match_index = np.argmin(face_distances)
-
-                    # If match found, update name and confidence
-                    if matches[best_match_index]:
-                        name = self.known_face_names[best_match_index]
-                        confidence = face_confidence(
-                            face_distances[best_match_index])
-
-                    logger.debug(name)
-                    logger.debug(confidence)
-
-                    # Append face name to list
-                    self.face_names.append(f"{name} ({confidence}%)")
-
-            self.process_current_frame = not self.process_current_frame
-            # Draw rectangles around faces
-            for (top, right, bottom, left), name in zip(reversed(self.face_locations), reversed(self.face_names)):
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
-
-                cv2.rectangle(frame, (left, top),
-                              (right, bottom), (0, 255, 0), 1)
-                # Display face name and confidence
-                cv2.putText(
-                    frame,
-                    name,
-                    (left + 6, bottom - 6),
-                    cv2.FONT_HERSHEY_DUPLEX,
-                    0.6,
-                    (255, 255, 255),
-                    1,
+                # Find matches
+                face_distances = face_recognition.face_distance(
+                    self.known_face_encodings, face_encoding
                 )
-            cv2.imshow("Cam", frame)
+                best_match_index = np.argmin(face_distances)
 
-            _, buffer = cv2.imencode('.jpg', frame)
-            frame_bytes = buffer.tobytes()
+                # If match found, update name and confidence
+                if matches[best_match_index]:
+                    name = self.known_face_names[best_match_index]
+                    confidence = face_confidence(face_distances[best_match_index])
 
-            yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n'
+                logger.debug(name)
+                logger.debug(confidence)
 
+                # Append face name to list
+                self.face_names.append(f"{name} ({confidence}%)")
 
-if __name__ == '__main__':
-    fr = FaceRecognition()
-    fr.run_recognition()
+                self.process_current_frame = not self.process_current_frame
+
+            return self.face_names
+        else:
+            return None
--- frontend/src/components/FaceRecognitionWindow.vue
+++ frontend/src/components/FaceRecognitionWindow.vue
@@ -9,11 +9,18 @@
       <div class="flex justify-between px-16 pt-6">
         <div class="flex flex-col items-center">
           <div
-            class="flex flex-col border-[1px] border-activeText w-[30vw] h-[67vh] rounded-xl"
-          ></div>
-          <div
-            class="w-24 h-24 border-[1px] border-activeText rounded-full mt-4"
-          ></div>
+            class="flex flex-col border-[1px] border-activeText w-[30vw] rounded-xl"
+          >
+            <video
+              class="h-full w-full rounded-xl"
+              ref="video"
+              autoplay
+            ></video>
+          </div>
+          <button
+            @click="takeScreenshot"
+            class="w-24 h-24 border-[3px] border-red-600 rounded-full mt-4 bg-neutral-400"
+          ></button>
         </div>
         <div
           class="flex flex-col border-[1px] justify-between border-activeText w-[35vw] h-[83vh] rounded-xl"
@@ -68,50 +75,38 @@ export default {
     },
   },
   methods: {
-    handleFilesUpload() {
-      this.uploadFile = this.$refs.files.files[0];
-      this.url = URL.createObjectURL(this.uploadFile);
-      this.files.push(this.uploadFile.name);
-      console.log(this.files);
-      if (this.uploadFile.type.startsWith("video")) {
-        this.isVideo = true;
-        this.isAudio = false;
-      } else if (this.uploadFile.type.startsWith("audio")) {
-        this.isAudio = true;
-        this.isVideo = false;
-      }
-    },
-    async submitFiles() {
-      this.isLoading = true;
-      const formData = new FormData();
-      formData.append("file", this.uploadFile);
-      console.log(formData);
+    takeScreenshot() {
+      // Capture screenshot from the video
+      const canvas = document.createElement("canvas");
+      canvas.width = this.$refs.video.videoWidth;
+      canvas.height = this.$refs.video.videoHeight;
+      canvas
+        .getContext("2d")
+        .drawImage(this.$refs.video, 0, 0, canvas.width, canvas.height);
+      const image = canvas.toDataURL("image/jpeg");
+
+      // Send the captured image to the server for face recognition
       axios
-        .post(
-          `http://${process.env.VUE_APP_AUDIOREC_SERVICE_IP}/audio_sample`,
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        )
-        .then((response) => {
-          this.text_massage = response.data.text;
-          this.text_language = response.data.language;
-          this.isLoading = false;
-          this.isReady = true;
-          console.log(this.text_massage);
-          console.log(this.text_language);
+        .post("http://localhost:8000/facerecognition", {
+          image: image,
         })
-        .catch(
-          function (error) {
-            console.log("Ошибка в обработке:", error);
-            this.isLoading = false;
-            this.isError = true;
-          }.bind(this)
-        );
+        .then((response) => {
+          console.log("Upload successful:", response.data);
+        })
+        .catch((error) => {
+          console.error("Error uploading file:", error);
+        });
     },
+  },
+  mounted() {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        this.$refs.video.srcObject = stream;
+      })
+      .catch((error) => {
+        console.log("Error accessing webcam:", error);
+      });
   },
 };
 </script>
